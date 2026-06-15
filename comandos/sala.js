de0c11ab891a4a15aa2e79a96ba5e5af07d7bb64
banidozzy-prog <banidozzy@gmail.com>
2026-06-15 01:56:14 -0300

    Update sala.js

--- comandos/sala.js
+++ comandos/sala.js
@@ -1,1 +1,8 @@
-
+module.exports = {
+    name: 'sala',
+    async execute(message, args, client) {
+        // Renomeia o canal
+        await message.channel.setName('pagar-2,00');
+        message.channel.send('✅ Sala criada! Canal configurado para pagamento.');
+    }
+};
