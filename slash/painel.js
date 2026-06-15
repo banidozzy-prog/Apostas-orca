5e07c0ba3c3814a518e21abb8cbabfc417187ff5
banidozzy-prog <banidozzy@gmail.com>
2026-06-15 02:35:33 -0300

    Update painel.js

--- slash/painel.js
+++ slash/painel.js
@@ -1,1 +1,14 @@
+const { SlashCommandBuilder } = require('discord.js');
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('painel')
+        .setDescription('Abre o painel de gerenciamento da Org'),
+    async execute(interaction) {
+        // Aqui você pode colocar o seu Embed profissional de painel
+        await interaction.reply({ 
+            content: '⚙️ **Painel de Controle da Org**\nSelecione uma opção abaixo:', 
+            ephemeral: true 
+        });
+    }
+};
