fda93167eaff54d889f8dbd412ddcbe392f43506
banidozzy-prog <banidozzy@gmail.com>
2026-06-15 01:57:14 -0300

    Update sw.js

--- comandos/sw.js
+++ comandos/sw.js
@@ -1,1 +1,8 @@
-
+module.exports = {
+    name: 'sw',
+    async execute(message, args, client) {
+        message.channel.send('📊 **Painel de Resultados:**\nEscolha o resultado da partida abaixo:', {
+            // Aqui você adicionaria os componentes (botões) depois
+        });
+    }
+};
