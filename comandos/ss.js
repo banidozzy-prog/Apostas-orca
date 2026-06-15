15388ec71d9712604b02e53413caab2da884e666
banidozzy-prog <banidozzy@gmail.com>
2026-06-15 01:58:42 -0300

    Update ss.js

--- comandos/ss.js
+++ comandos/ss.js
@@ -1,1 +1,7 @@
-
+module.exports = {
+    name: 'ss',
+    async execute(message, args, client) {
+        const tipo = args[0]; // mob ou emu
+        message.channel.send(`🔎 **Chamando analista de SS (${tipo})...**\nAguarde a equipe técnica.`);
+    }
+};
