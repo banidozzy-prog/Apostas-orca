10fbb779e49030de907b4a97d858ae9cc7cc56e7
banidozzy-prog <banidozzy@gmail.com>
2026-06-15 17:05:34 -0300

    Delete comandos/p.js

--- comandos/p.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    name: 'p',
-    async execute(message, args, client) {
-        message.channel.send('🏆 **Ranking/Perfil:**\nCarregando estatísticas do jogador...');
-    }
-};
