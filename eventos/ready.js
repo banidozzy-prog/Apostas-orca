25f5d99b51d96d57e0aeaf1d5557c0c176999e40
banidozzy-prog <banidozzy@gmail.com>
2026-06-15 17:25:28 -0300

    Update ready.js

--- eventos/ready.js
+++ eventos/ready.js
@@ -1,7 +1,8 @@
 module.exports = {
     name: 'ready',
     once: true,
-    execute(client) {
-        console.log(`Bot conectado como ${client.user.tag}!`);
+    async execute(client) {
+        console.log(`✅ Bot conectado como ${client.user.tag}!`);
+        client.user.setActivity('Gerenciando Apostas', { type: 'PLAYING' });
     }
 };
