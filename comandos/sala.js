ffbdbfae38af207573b2dcc64fcc9af9d60471a5
banidozzy-prog <banidozzy@gmail.com>
2026-06-15 02:05:16 -0300

    Update sala.js

--- comandos/sala.js
+++ comandos/sala.js
@@ -1,8 +1,24 @@
+const { ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+
 module.exports = {
     name: 'sala',
     async execute(message, args, client) {
         // Renomeia o canal
         await message.channel.setName('pagar-2,00');
-        message.channel.send('✅ Sala criada! Canal configurado para pagamento.');
+
+        // Cria o botão
+        const row = new ActionRowBuilder()
+            .addComponents(
+                new ButtonBuilder()
+                    .setCustomId('ver_pix') // Este ID tem que ser igual ao do interactionCreate.js
+                    .setLabel('Ver PIX')
+                    .setStyle(ButtonStyle.Primary)
+                    .setEmoji('1513764622677573714') // ID do seu emoji PIX
+            );
+
+        message.reply({ 
+            content: '✅ **Sala criada!** Clique no botão abaixo para ver os dados de pagamento:', 
+            components: [row] 
+        });
     }
 };
