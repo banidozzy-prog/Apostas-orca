fd9d9acad01b9c6ae280a80b843f922a81e5501c
banidozzy-prog <banidozzy@gmail.com>
2026-06-15 01:59:00 -0300

    Update p.js

--- comandos/p.js
+++ comandos/p.js
@@ -1,1 +1,6 @@
-
+module.exports = {
+    name: 'p',
+    async execute(message, args, client) {
+        message.channel.send('🏆 **Ranking/Perfil:**\nCarregando estatísticas do jogador...');
+    }
+};
