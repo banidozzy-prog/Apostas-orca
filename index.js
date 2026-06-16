e227e8c91c93006a6d29e1708cf1503a80f56694
banidozzy-prog <banidozzy@gmail.com>
2026-06-15 21:29:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-// Blinda o terminal contra avisos de depreciação do Discord.js
-process.removeAllListeners('warning');
-process.on('warning', (warning) => {
-    if (warning.name !== 'DeprecationWarning') console.warn(warning);
-});
-
+// index.js corrigido (Sintaxe correta)
 const { Client, GatewayIntentBits, Collection } = require('discord.js');
 const fs = require('fs');
 
@@ -19,31 +14,17 @@ const client = new Client({
 client.comandos = new Collection();
 client.slashCommands = new Collection();
 
-// Carregar Slash Commands (Pasta /barra)
-const slashFiles = fs.readdirSync('./barra').filter(file => file.endsWith('.js'));
-for (const file of slashFiles) {
-    const command = require(`./barra/${file}`);
-    client.slashCommands.set(command.data.name, command);
-}
-
-// Carregar Comandos de Prefixo (Pasta /comandos)
-const cmdFiles = fs.readdirSync('./comandos').filter(file => file.endsWith('.js'));
-for (const file of cmdFiles) {
-    const command = require(`./comandos/${file}`);
-    client.comandos.set(command.name, command);
-}
-
-// Carregar Eventos (Pasta /eventos)
-const eventFiles = fs.readdirSync('./eventos').filter(file => file.endsWith('.js'));
-for (const file of eventFiles) {
-    const event = require(`./eventos/${file}`);
-    
-    // Vincula o evento. Se for clientReady, usa o nome correto para evitar avisos
-    if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args, client));
-    } else {
-        client.on(event.name, (...args) => event.execute(...args, client));
+// Carregamento de comandos e eventos
+const loadFiles = (dir) => {
+    const files = fs.readdirSync(`./${dir}`).filter(file => file.endsWith('.js'));
+    for (const file of files) {
+        const item = require(`./${dir}/${file}`);
+        if (dir === 'barra') client.slashCommands.set(item.data.name, item);
+        else if (dir === 'comandos') client.comandos.set(item.name, item);
     }
-}
+};
+
+loadFiles('barra');
+loadFiles('comandos');
 
 client.login('SEU_TOKEN_AQUI');
