e79f311d56950982bcddf2674b9a21beafe8cd36
banidozzy-prog <banidozzy@gmail.com>
2026-06-17 14:19:27 -0300

    Update fila.js

--- ORCA-BOT/fila.js
+++ ORCA-BOT/fila.js
@@ -1,3 +1,29 @@
 module.exports = {
-  name: "fila"
+
+    name: "fila",
+
+    execute(message) {
+
+        message.channel.send(`
+<a:money:1513764000431865988> FILAS ORCA
+
+📱 Mobile
+
+<:emulador:1513764827594493972> Emulador
+
+⚔️ Misto
+
+🎯 Tático
+
+<:gelo:1513246422865019040> Gelo Normal
+
+<:gelo:1513246422865019040> Gelo Infinito
+
+<:entrar:1514854758693863424> Entrar
+
+<:sairr:1514511377958441076> Sair
+        `);
+
+    }
+
 };
