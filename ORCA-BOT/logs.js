c0e66684c5aa3da18d8be230b80098af4334acbb
banidozzy-prog <banidozzy@gmail.com>
2026-06-17 14:18:36 -0300

    Update logs.js

--- ORCA-BOT/logs.js
+++ ORCA-BOT/logs.js
@@ -1,3 +1,23 @@
 module.exports = {
-  name: "logs"
+
+    name: "logs",
+
+    execute(message) {
+
+        message.channel.send(`
+<:Engrenagem:1513231224385310882> CONFIGURAÇÃO DE LOGS
+
+📋 Logs de Partidas
+
+📋 Logs de SS
+
+📋 Logs de Delay
+
+📋 Logs de Blacklist
+
+📋 Logs de PIX
+        `);
+
+    }
+
 };
