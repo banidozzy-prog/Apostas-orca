f8c5c0f8271322c9b4fba6795de4cf8226c70377
banidozzy-prog <banidozzy@gmail.com>
2026-06-17 14:17:20 -0300

    Update unb.js

--- ORCA-BOT/unb.js
+++ ORCA-BOT/unb.js
@@ -1,3 +1,23 @@
 module.exports = {
-  name: "unb"
+
+    name: "unb",
+
+    execute(message, args) {
+
+        const user = message.mentions.users.first();
+
+        if (!user) {
+            return message.reply(
+                "Use: +unb @usuario"
+            );
+        }
+
+        message.channel.send(`
+<a:corfimarr:1513767887368880218>
+
+${user} removido da blacklist.
+        `);
+
+    }
+
 };
