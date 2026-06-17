fb9b6b4761fc2a5de2e3e1c2dd733bd06323a630
banidozzy-prog <banidozzy@gmail.com>
2026-06-17 14:16:07 -0300

    Update sw.js

--- ORCA-BOT/sw.js
+++ ORCA-BOT/sw.js
@@ -1,3 +1,31 @@
 module.exports = {
-  name: "sw"
+
+    name: "sw",
+
+    execute(message, args) {
+
+        const user = message.mentions.users.first();
+
+        if (!user) {
+            return message.reply(
+                "Use: +sw @usuario"
+            );
+        }
+
+        message.channel.send(`
+🏆 PAINEL DA PARTIDA
+
+Mediador: ${message.author}
+
+Jogador Marcado: ${user}
+
+🏆 Vitória Normal
+
+<:wo:1514083677880057916> Vitória por W.O
+
+<:finalizar:1514083336660975848> Finalizar Aposta
+        `);
+
+    }
+
 };
