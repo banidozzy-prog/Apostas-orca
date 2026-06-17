7528c4656ba309cbd3d7f2a6770f71f739e92b4f
banidozzy-prog <banidozzy@gmail.com>
2026-06-17 14:17:42 -0300

    Update vb.js

--- ORCA-BOT/vb.js
+++ ORCA-BOT/vb.js
@@ -1,3 +1,25 @@
 module.exports = {
-  name: "vb"
+
+    name: "vb",
+
+    execute(message, args) {
+
+        const id = args[0];
+
+        if (!id) {
+            return message.reply(
+                "Use: +vb ID"
+            );
+        }
+
+        message.channel.send(`
+<:blacklist:1514507237119365140>
+
+Consulta realizada.
+
+ID pesquisado: ${id}
+        `);
+
+    }
+
 };
