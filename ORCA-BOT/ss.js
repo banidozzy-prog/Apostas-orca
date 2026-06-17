03ad5590ee36e9e7d954df084511d70417521707
banidozzy-prog <banidozzy@gmail.com>
2026-06-17 14:16:30 -0300

    Update ss.js

--- ORCA-BOT/ss.js
+++ ORCA-BOT/ss.js
@@ -1,3 +1,30 @@
 module.exports = {
-  name: "ss"
+
+    name: "ss",
+
+    execute(message, args) {
+
+        const modo = args[0];
+        const analise = args.slice(1).join(" ");
+
+        if (!modo || !analise) {
+            return message.reply(
+                "Use: +ss mob análise 7"
+            );
+        }
+
+        message.channel.send(`
+<:ss:1513765098861105152> SOLICITAÇÃO DE ANÁLISE
+
+👤 Mediador: ${message.author}
+
+📱 Modo: ${modo}
+
+🔊 Call: ${analise}
+
+⚠️ Comparecer imediatamente.
+        `);
+
+    }
+
 };
