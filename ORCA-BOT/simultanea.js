6757b4e8dc8ffced2854bb5625c18bc32da8548a
banidozzy-prog <banidozzy@gmail.com>
2026-06-17 14:18:12 -0300

    Update simultanea.js

--- ORCA-BOT/simultanea.js
+++ ORCA-BOT/simultanea.js
@@ -1,3 +1,27 @@
 module.exports = {
-  name: "simultanea"
+
+    name: "simultanea",
+
+    execute(message) {
+
+        message.channel.send(`
+<:simultnea:1514876848553725993> **PAINEL DE FILAS**
+
+📊 Estatísticas atuais
+
+📱 Mobile: 0
+
+<:emulador:1513764827594493972> Emulador: 0
+
+⚔️ Misto: 0
+
+🎯 Tático: 0
+
+🏆 Finalizadas: 0
+
+🔄 Em andamento: 0
+        `);
+
+    }
+
 };
