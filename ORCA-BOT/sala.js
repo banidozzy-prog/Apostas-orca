519f9d143b01517d069287283fd498da6a62f43c
banidozzy-prog <banidozzy@gmail.com>
2026-06-17 15:24:49 -0300

    Update sala.js

--- ORCA-BOT/sala.js
+++ ORCA-BOT/sala.js
@@ -1,30 +1,32 @@
+// sala.js
+
 module.exports = {
+name: "sala",
 
-    name: "sala",
+execute(message, args) {
 
-    execute(message, args) {
+    const id = args[0];
+    const senha = args[1];
 
-        const id = args[0];
-        const senha = args[1];
+    if (!id || !senha) {
+        return message.reply(
+            "❌ Use: +sala (id) (senha)"
+        );
+    }
 
-        if (!id || !senha) {
-            return message.reply(
-                "<:cancelar:1514876408428494962> Use: +sala (id) (senha)"
-            );
-        }
+    message.channel.send(`
 
-        message.channel.send(`
-<:sala:1513246424320577586> **SALA LIBERADA**
+<:sala:1513246424320577586> SALA LIBERADA
 
-<:ID:1514877094163779644> **ID DA SALA:** ${id}
+<:ID:1514877094163779644> ID: ${id}
 
-🔑 **SENHA:** ${senha}
+<:copiar:1514876703028019291> SENHA: ${senha}
 
-<a:corfimarr:1513767887368880218> Os pagamentos foram confirmados.
+<a:corfimarr:1513767887368880218> Pagamentos confirmados.
 
 ⚠️ Entrem na sala e iniciem a partida.
-        `);
+`);
 
-    }
+}
 
 };
